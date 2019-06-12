5b064a2597f3c7e6693aca154f3d126f245e9e74
clickhirphae <hpclickcomputerhope@gmail.com>
2019-06-12 17:01:01 -0400

    first commit

---
+++ StringAssignment.java
@@ -0,0 +1,9 @@
+
+import java.util.*;
+public class StringAssignment {
+    public static void main(String args[]){
+
+        String Name="Hirpha";
+        System.out.println("My name is:"+Name);
+    }
+}
